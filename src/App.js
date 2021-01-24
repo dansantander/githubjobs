f7d533a23fd8ca660103076475b32d67147a69f1
dansantander <dansantandr@gmail.com>
2021-01-24 11:49:49 -0600

    chore: dispatching get data action

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Container } from 'react-bootstrap';
 import useFetchJobs from './useFetchJobs';
 
 function App() {
-  return <h1> Hello World! </h1>
+  const { jobs, loading, error } = useFetchJobs();
+  return (
+    <Container>
+      { loading && <h1>Loading</h1> }
+      { error && <h1>Error. Try Refreshing.</h1> }
+      <h1>{jobs.length}</h1>
+    </Container>
+  );
 }
 
 export default App;
