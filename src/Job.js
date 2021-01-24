9bfcbd5ce7ee2005573912d3afe93e58f279e2f6
dansantander <dansantandr@gmail.com>
2021-01-24 15:34:36 -0600

    chore: add bootstrap card to job component

--- src/Job.js
+++ src/Job.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 /* eslint-disable */
 function Job({ job }) {
   return (
-    <div>
-      {job.title}
-    </div>
+    <Card>
+      <Card.Body>
+        <div className="justify-content-between">
+          <div>
+            <Card.Title>
+              {job.title} - <span className="text-muted font-weight-light">{job.company}</span>
+            </Card.Title>
+          </div>
+
+        </div>
+      </Card.Body>
+    </Card>
   );
 }
 
